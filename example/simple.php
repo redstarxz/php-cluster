a3924ffac8a061d64addc10474dab59ebd1cc217
hfcorriez <hfcorriez@gmail.com>
2013-09-05 23:37:04 +0800

    Add advance example

--- example/simple.php
+++ example/simple.php
@@ -4,28 +4,13 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 declare(ticks = 1);
 
-$cluster = new \Pagon\Cluster(array(
-    'pids_dir'     => true,
-    'auto_restart' => true
-));
+$cluster = new \Pagon\Cluster();
 
 if ($cluster->isMaster()) {
-    $cluster->on('exit', function (\Pagon\Worker $worker, $code) use ($cluster) {
-        echo getmypid() . ':' . ' worker exited: ' . $code . PHP_EOL;
-        //$worker->restart();
-    });
-
-    for ($i = 0; $i < 5; $i++) {
-        $cluster->fork(__FILE__);
-    }
+    $cluster->fork(__FILE__);
 
     $cluster->run();
 } else {
-    /** @var $process \Pagon\Process */
-    $process->on('exit', function () {
-        echo getmypid() . ':' . ' i am quit' . PHP_EOL;
-    });
-
     echo getmypid() . ':' . ' i am work' . PHP_EOL;
     sleep('5');
 }
